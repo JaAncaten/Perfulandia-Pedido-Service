f31f685d631282e920e0446f0647454c53f0a6e4
JaAncaten <ja.ancaten@duocuc.cl>
2026-06-24 14:27:23 -0400

    Servivico de Pedido funcional y ejecutable

---
+++ src/main/java/com/perfulandia/pedido/dto/PedidoResponse.java
@@ -0,0 +1,5 @@
+package com.perfulandia.pedido.dto;
+
+public class PedidoResponse {
+    
+}
---
+++ src/main/java/com/perfulandia/pedido/model/DetallePedido.java
@@ -0,0 +1,33 @@
+package com.perfulandia.pedido.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "detalle_pedidos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DetallePedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetalle;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_pedido", nullable = false)
+    private Pedido pedido;
+
+    @Column(nullable = false)
+    private Long idProducto;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false)
+    private Integer precioUnitario;
+
+    @Column(nullable = false)
+    private Integer subtotal;
+}
